3628de3c7ac0fae650ec437a3d5e19dfc6f5eb74
mauro <maurobilardo02@gmail.com>
2025-09-09 15:38:56 +0200

    esercizio iniziale fatto alla meno peggio

---
+++ script/script.js
@@ -0,0 +1,28 @@
+let frutta = ["mela", "banana", "pera"];
+
+function stampa(){
+    let lista = document.getElementById("listaIniziale");
+    let banana = document.getElementById("banana"); 
+    let kiwi = document.getElementById("kiwi");
+
+    // lista normale
+    for(let oggetto of frutta){
+        let li = document.createElement("li");
+        li.textContent = oggetto;
+        lista.appendChild(li);
+    }
+
+    // lista con solo il secondo frutto
+    let secondo = document.createElement("li");
+    secondo.textContent = frutta[1]
+    banana.appendChild(secondo);
+
+    frutta.splice(1,1, "kiwi");
+    for(let oggetto of frutta){
+        let terzo = document.createElement("li");
+        terzo.textContent = oggetto;
+        kiwi.appendChild(terzo);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", stampa());
